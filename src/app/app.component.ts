5783a0d02e5647486da026bfb064e47c5bbe83eb
GeoSomu <somu.career2420@gmail.com>
2021-10-17 12:47:45 +0530

    added ga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { GoogleAnalyticsService } from './google-analytics.service';
+declare let ga: Function;
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-CRUD-Operations';
+
+  constructor(public router: Router, public googleAnalytics : GoogleAnalyticsService){
+
+    this.router.events.subscribe( event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    })
+  }
+
+  onClikLoginURL() {
+    this.googleAnalytics.eventEmitter('login','clicked','userLogin', 1);
+  }
+
+  onClickRegURL(){
+    this.googleAnalytics.eventEmitter('reg', 'clicked', 'userReg', 1);
+  }
+
+  onClickAdminURL(){
+    this.googleAnalytics.eventEmitter('admin', 'clicked', 'adminLogin', 1);
+  }
 }
